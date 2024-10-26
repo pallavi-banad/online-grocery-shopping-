d7e1bdc969eef87ae0e23765cdd28259b70bef57
pallavi-banad <pallavi.banad@gmail.com>
2024-10-26 16:52:55 +0530

    Delete logout.js

--- logout.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('homeButton').addEventListener('click', function() {
-    // Clear any session data
-    localStorage.removeItem('user'); // Adjust according to your storage method
-    
-    // Redirect to home page
-    window.location.href = 'index.html'; // Change to your home page
-});
